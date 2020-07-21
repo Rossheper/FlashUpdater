59eeb1a375925f80522c5afd4fb85e92b70a4c17
Vadim Slepov <13svg@constr.kbp>
2020-07-21 10:34:01 +0300

    Реализовал класс-обертку (переход с небезопасного (неуправляемого) до безопасного(управляемого) кода)  для базового метода класса Manchester.

---
+++ UFA.MILSTD1553/MILSTD1553Operation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TKB995STD1553B;
+namespace UFA.MILSTD1553
+{
+    /// <summary>
+    /// Класс-обертка для работы с интерфейсом MILSTD1553
+    /// </summary>
+    public class MILSTD1553Operation : Manchester
+    {
+        #region Конструкторы
+        public MILSTD1553Operation() : base() { }
+        #endregion
+
+        #region Открытые методы
+        /// <summary>
+        /// Метод, реализующий метод базового класса в управляемом виде
+        /// </summary>
+        /// <param name="oldAddr">Старый адрес</param>
+        /// <param name="addrOy">Текущий адрес</param>
+        /// <param name="K">Направление передачи (К = 0 - передача (Ф1), К = 1 - прием (Ф2))</param>
+        /// <param name="subAddr">Подадрес внутри устройства</param>
+        /// <param name="ncd">Количество слов данных для передачи</param>
+        /// <param name="massToSend">Одномерный массив с данными для передачи/приема</param>
+        /// <returns></returns>
+        public bool SendData(short oldAddr, short addrOy, short K, short subAddr, short ncd, ushort[] massToSend)
+        {
+            unsafe
+            {
+                fixed (ushort* ptr = &massToSend[0])
+                {
+                    return base.sendData(oldAddr, addrOy, K, subAddr, ncd, ptr);
+                }
+            }
+        }
+        #endregion
+    }
+}
