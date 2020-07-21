8d79b729fb8035c2d8532702995e50fb9783f03a
Vadim Slepov <13svg@constr.kbp>
2020-07-21 12:33:24 +0300

    Реализовал частное исключение для проверки корректности файла IntelHex при его чтении

---
+++ UFA.Exceptions/IntelHexFileCheckException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFA.Exceptions
+{
+    [Serializable]
+    public class IntelHexFileCheckException : ApplicationException
+    {
+        public IntelHexFileCheckException() { }
+        public IntelHexFileCheckException(string message) : base(message) { }
+        public IntelHexFileCheckException(string message, Exception inner)
+            : base(message, inner) { }
+        public IntelHexFileCheckException(System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            :base (info,context) { }
+    }
+}
